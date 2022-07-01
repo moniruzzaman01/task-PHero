4bac4f287391ed343618ba34419054716a104f87
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-07-01 11:36:23 +0600

    login implimented

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,40 @@
 import React from "react";
 
 const Login = () => {
+  const handleForm = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const pass = event.target.pass.value;
+    console.log(email, pass);
+    const data = { email, pass };
+
+    fetch(`http://localhost:5000/login`, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success === true) {
+          event.target.reset();
+        } else {
+          // handle error
+        }
+      });
+  };
   return (
     <div className=" min-h-[60vh] flex justify-center items-center ">
       <div className=" w-full">
         <h2 className=" text-3xl mb-10 font-bold">Login</h2>
-        <form onSubmit={"handleForm"} action="" className=" w-2/3 mx-auto">
+        <form onSubmit={handleForm} action="" className=" w-2/3 mx-auto">
           <input
             type="email"
             name="email"
             placeholder=" Email"
-            //   onChange={(event) => setEmail(event.target.value)}
             className=" w-full mb-5 bg-gray-100"
             required
           />
@@ -18,7 +42,6 @@ const Login = () => {
             type="password"
             name="pass"
             placeholder=" Password"
-            // onChange={(event) => setBill(event.target.value)}
             className=" w-full mb-5 bg-gray-100"
             required
           />
